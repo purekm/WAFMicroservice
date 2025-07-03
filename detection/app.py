d3a2adf747b99462fc59a623644663376f4c4600
purekm <rudals4349@naver.com>
2025-07-03 16:33:15 +0900

    Jenkins연동확인2

--- detection/app.py
+++ detection/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Github -> Jenkins -> Docker 연동 완료 되었나요?!"
+    return "Github -> Jenkins -> Docker 이제 진짜 연동 되었나요?!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
